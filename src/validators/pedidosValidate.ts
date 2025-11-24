5b5e7b494b17429f1e0f27215d26afb2308f4841
AstraAbigail <astrabigail@gmail.com>
2025-11-24 16:28:44 -0300

    zod saque todas las validaciones del lenght

--- src/validators/pedidosValidate.ts
+++ src/validators/pedidosValidate.ts
@@ -2,10 +2,10 @@ import { z } from "zod"
 
 
   const schema = z.object({
-    cliente: z.string().length(20),
+    cliente: z.string(),
     dniCliente:z.number().positive().min(8,"Debe tener 8 caracteres"),
-    direccion:z.string().length(30),
-    tecnicoAsignado: z.string().length(15),
+    direccion:z.string(),
+    tecnicoAsignado: z.string(),
     fechaProgramada:z.string(),
     estado:z.string()
   })
